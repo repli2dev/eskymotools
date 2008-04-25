df47db655ccd50160fd2b423860af47c420a5b9e
jan.papousek <jan.papousek@28578956-b94b-0410-bc83-bd28d5b7d619>
2008-04-25 22:43:56 +0000

    Odstraneny veci navic.
    git-svn-id: http://eskymofw.googlecode.com/svn/trunk@5 28578956-b94b-0410-bc83-bd28d5b7d619

--- MySQL.php
+++ MySQL.php
@@ -11,29 +11,29 @@
 class MySQL {
 	
 	/**
-	* @var boolean Promenna, ktera urcuje, zda se testuje (TRUE) nebo ne (FALSE). 
+	* @var boolean Promenna, ktera urcuje, zda je trida v testovacim rezimu (TRUE) nebo ne (FALSE). 
 	*/
-	private static $test;
+	private static $test = TRUE;
 
 	/**
 	* @var string Server, na kterem bezi databaze.
 	*/
-	private static $server;
+	private static $server = "localhost";
 	
 	/**
 	* @var string Nazev databaze, se kterou pracuji.
 	*/
-	private static $database;
+	private static $database = "";
 
 	/**
 	* @var string Jmeno uzivatele databaze.
 	*/
-	private static $user;
+	private static $user = "";
 
 	/**
 	* @var string Heslo k databazi.
 	*/
-	private static $password;
+	private static $password = "";
 
 	/**
 	* @var string Porovnavani spojeni.
@@ -52,27 +52,150 @@ class MySQL {
 	* @return boolean
 	*/
 	public static function connect() {
-		if (MySQL :: getTest()) {		
-			mysql_pconnect(MySQL :: getServer(),MySQL :: getUser(), MySQL :: getPassword()) or die(__FILE__." : ".__LINE__." : Chyba v pripojeni k databazovemu serveru");
-			mysql_select_db(MySQL :: getDatabase()) or die(__FILE__." : ".__LINE__." : Chyba v pripojeni k databazi")
-			mysql_query("SET CHARACTER SER ". MySQL :: getCharacter()) or die(__FILE__." : ".__LINE__." : Chyba v nastaveni porovnavani");
+		if (self :: getTest()) {		
+			mysql_pconnect(MySQL :: getServer(),MySQL :: getUser(), MySQL :: getPassword()) or die("Error: " . __FILE__ . " : " . __LINE__ . " : Chyba v pripojeni k databazovemu serveru");
+			mysql_select_db(MySQL :: getDatabase()) or die("Error: " . __FILE__." : ".__LINE__." : Chyba v pripojeni k databazi")
+			mysql_query("SET CHARACTER SET ". MySQL :: getCharacter()) or die("Error: " . __FILE__ . " : " . __LINE__ . " : Chyba v nastaveni porovnavani");
 			return TRUE;
 		}
 		else {
 			try {
-				if (!(mysql_pconnect(MySQL :: getServer(),MySQL :: getUser(), MySQL :: getPassword()))) {
+				if (!(mysql_pconnect(MySQL :: getServer(),MySQL :: getUser(), MySQL :: $password))) {
 					throw new Exception;
 				}
 				if (!(mysql_select_db(MySQL :: getDatabase()))) {
-					mysql_select_db(MySQL :: getDatabase());
+					throw new Exception;
 				}
 				if (!(mysql_query("SET CHARACTER SER ". MySQL :: getCharacter()))) {
 					throw new Exception;
 				}
+				return TRUE
 				
-			}			
+			}
+			catch (Exception $e) {
+				return FALSE;
+			}
 		}
 	}
 	
+	
+	/**
+	* Vrati databazovy server.
+	* @return string
+	*/
+	public static function getServer() {
+ 		return self :: $server;
+	}
+	
+	/**
+	* Vrati nazev databaze.
+	* @return string
+	*/	
+	public static function getDatabase() {
+		return self :: $database;
+	}
+	
+	/**
+	* Vrati jmeno uzivatele databaze.
+	* @return string 
+	*/
+	public static function getUser() {
+		return self :: $user;
+	} 
+	
+	/**
+	* Vrati pouzitou znakovou sadu pripojeni (napr. utf8).
+	* @return string
+	*/
+	public static function getCharacter() {
+		return self :: $character;
+	}
+	
+	/**
+	* Vrati TRUE, pokud je trida v testovacim rezimu.
+	* @return boolean
+	*/	
+	public static function getTest() {
+		return self :: $test;
+	}
+	
+	/**
+	* Nastavi databazovy server.
+	* @param string
+	* @return void
+	*/
+	public static function setServer($server) {
+		self :: $server = $server;
+	}
+	
+	/**
+	* Nastavi nazev databaze.
+	* @param string
+	* @return void
+	*/
+	public static function setDatabase($database) {
+		self :: $database = $database;
+	}
+	
+	/**
+	* Nastavi jmeno uzivatele.
+	* @param string
+	* @return void
+	*/
+	public static function setUser($user) {
+		self :: $user = $user;
+	}
+	
+	/**
+	* Nastavi heslo.
+	* @param string
+	* @return void
+	*/
+	public static function setPassword($password) {
+		self :: $password = $password;
+	}
+
+	/**
+	* Nastavi znakovou sadu (porovnavani) pripojeni (napr. utf8).
+	* @param string
+	* @return void
+	*/
+	public static function setCharacter($character) {
+		self :: $character = $character;
+	}
+	
+	/**
+	* Nastavi testovaci rezim.
+	* @param boolean
+	* @return void
+	*/	
+	public static function setTest($test) {
+		self :: $test = $test;
+	}
+	
+	/**
+	* Polozi dotaz databazi.
+	* @param string SQL dotaz.
+	* @param string Soubor, ktery vola metodu.
+	* @param int Radek, ze ktereho je metoda volana.
+	* @return boolean
+	*/
+	public static function query($sql,$file = NULL, $line = NULL) {
+		if (self :: $test) {
+			mysql_query($sql) or die ("Error: " . $file . " : " . $line . " : " . $sql);
+			return TRUE
+		}
+		else {
+			try {
+				if (!(mysql_query($sql)) {
+					throw new Exception;
+				}
+				return TRUE;
+			}
+			catch (Exception $e) {
+				return FALSE;
+			}
+		}
+	}
 }
 ?>
