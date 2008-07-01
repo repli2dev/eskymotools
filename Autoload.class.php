1d70d0800cdb5b74c311e7e29dd12646396b7a12
repli2dev <repli2dev@28578956-b94b-0410-bc83-bd28d5b7d619>
2008-07-01 08:40:14 +0000

    Autoload nacita automaticky i adresare prvni urovne
    git-svn-id: http://eskymofw.googlecode.com/svn/trunk@37 28578956-b94b-0410-bc83-bd28d5b7d619

--- Autoload.class.php
+++ Autoload.class.php
@@ -32,7 +32,27 @@ class Autoload {
 	* @return void
 	*/
 	public static function add($dir) {
+		//TODO: Kontrola zdvojenych polozek
 		self::$directory[] = "./" . $dir . "/";
+		//pridani vcetne podadresaru na prvni urovni (vice souboru by mohlo zpusobit chybu prilis mnoha otevrenych souboru)
+		$temp = explode("/",$dir);
+		if(empty($temp[1])){
+			$subdir = dir("./" . $dir . "/");
+			while (false !== ($entry = $subdir->read())) {
+				//kontrola typu
+				if(filetype("./".$dir."/".$entry."") == "dir" && ($entry != "." || $entry != "..")){
+					//kontrola pritomnosti souboru koncicich .class.php
+					$subdir2 = dir("./".$dir."/".$entry."");
+					while (false !== ($entry2 = $subdir2->read())) {
+						if(strpos($entry2,self::$ending) !== false && ($entry2 != "." || $entry2 != "..")){
+							self::add("./".$dir."/".$entry."");
+						}
+					}
+					$subdir2->close();
+				}
+			}
+			$subdir->close();
+		}
 	}
 
 	/**
