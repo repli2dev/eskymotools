58659cf0b50fd50a9a761658716c1c170b4f02e7
jan.papousek <jan.papousek@28578956-b94b-0410-bc83-bd28d5b7d619>
2009-08-04 21:27:12 +0000

    Licence
    git-svn-id: http://eskymofw.googlecode.com/svn/trunk@62 28578956-b94b-0410-bc83-bd28d5b7d619

--- eskymo/exceptions/DataNotFoundException.php
+++ eskymo/exceptions/DataNotFoundException.php
@@ -1,8 +1,19 @@
 <?php
 /**
+ * This source file is subject to the "New BSD License".
+ *
+ * For more information please see http://code.google.com/p/eskymofw/
+ *
+ * @copyright	Copyright (c) 2009 Jan Papoušek (jan.papousek@gmail.com),
+ *				Jan Drábek (repli2dev@gmail.com)
+ * @license		http://www.opensource.org/licenses/bsd-license.php
+ * @link		http://code.google.com/p/eskymofw/
+ */
+
+/**
  * This exception means the situation when the resource is not found.
  *
  * @author      Jan Papousek
- * @version     2009-07-08
+ * @version     $Id$
  */
 class DataNotFoundException extends Exception {}
