1c0ccd3d35b5e3528271cbcf5f03b6497901ffc5
jan.papousek <jan.papousek@28578956-b94b-0410-bc83-bd28d5b7d619>
2009-08-04 21:24:36 +0000

    Licence
    git-svn-id: http://eskymofw.googlecode.com/svn/trunk@61 28578956-b94b-0410-bc83-bd28d5b7d619

--- eskymo/io/files/FileNameFilter.php
+++ eskymo/io/files/FileNameFilter.php
@@ -1,8 +1,20 @@
 <?php
 /**
+ * This source file is subject to the "New BSD License".
+ *
+ * For more information please see http://code.google.com/p/eskymofw/
+ *
+ * @copyright	Copyright (c) 2009 Jan Papoušek (jan.papousek@gmail.com),
+ *				Jan Drábek (repli2dev@gmail.com)
+ * @license		http://www.opensource.org/licenses/bsd-license.php
+ * @link		http://code.google.com/p/eskymofw/
+ */
+
+/**
  * The file name filter.
  *
  * @author      Jan Papousek
+ * @version		$Id$
  * @see         IFileFilter
  */
 class FileNameFilter extends /*Nette\*/Object implements IFileFilter
