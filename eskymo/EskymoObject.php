d49228d745909508621b4a43d98556bc3e1b3e59
jan.papousek <jan.papousek@28578956-b94b-0410-bc83-bd28d5b7d619>
2009-08-11 11:55:44 +0000

    [Base] Do EskymoObject pridano getVars
    git-svn-id: http://eskymofw.googlecode.com/svn/trunk@78 28578956-b94b-0410-bc83-bd28d5b7d619

--- eskymo/EskymoObject.php
+++ eskymo/EskymoObject.php
@@ -10,12 +10,21 @@ class EskymoObject extends Object
 	public function  __construct() {}
 
 	/**
-	 * It returns objects methods
+	 * It returns object methods
 	 *
 	 * @return array Method names
 	 */
 	public function getMethods() {
 		return get_class_methods($this->getClass());
+	}
+
+	/**
+	 * It returns object variables
+	 *
+	 * @return array Variable names
+	 */
+	public function getVars() {
+		return get_class_vars($this->getClass());
 	}
 
 	/**
