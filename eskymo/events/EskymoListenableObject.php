7712c2b3af54c6f2728cbab8784e3efcd0db086d
jan.papousek <jan.papousek@28578956-b94b-0410-bc83-bd28d5b7d619>
2009-11-18 12:34:56 +0000

    Priadano tridy a rozhrani pro udalosti
    git-svn-id: http://eskymofw.googlecode.com/svn/trunk@127 28578956-b94b-0410-bc83-bd28d5b7d619

---
+++ eskymo/events/EskymoListenableObject.php
@@ -0,0 +1,37 @@
+<?php
+
+class EskymoEventObject extends EskymoObject implements IListenable
+{
+
+	/** @var array */
+	private $listeners = array();
+
+	/**
+	 * It adds a new listner which listens to the specified event type
+	 *
+	 * @param string $type Event type
+	 * @param IListener $listener
+	 */
+	protected final function addListener($type, IListener &$listener) {
+		if (empty($type)) {
+			throw new NullPointerException("type");
+		}
+		if (!isset($this->listners[$type])) {
+			$this->listners[$type] = array();
+		}
+		$this->listners[$type][] = $listener;
+	}
+
+	/**
+	 * It calls all listeners which listen to the specified
+	 */
+	protected final function callListeners($type, IEvent &$event) {
+		if (empty($type)) {
+			throw new NullPointerException("type");
+		}
+		foreach($this->listners AS $listener) {
+			$listener->listern($event);
+		}
+	}
+
+}
